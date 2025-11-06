174fd4fea37034d5ec966fc7fc5f7919fcbd3af2
Suraj Prasad-13 <spyadav0413@gmail.com>
2025-11-06 18:11:49 +0530

    added top and search bar

---
+++ src/components/Layout/Topbar.tsx
@@ -0,0 +1,31 @@
+import { IoLogoInstagram } from "react-icons/io";
+import { RiTwitterXLine } from "react-icons/ri";
+import { TbBrandMeta } from "react-icons/tb";
+
+const Topbar = () => {
+  return (
+    <div className="bg-rabbit-red text-white">
+      <div className="container mx-auto flex justify-between items-center py-3 px-4">
+        <div className="hidden md:flex items-center space-x-4">
+          <a href="#" className="hover:text-gray-300">
+            <TbBrandMeta className=" size-5" />
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            <IoLogoInstagram className=" size-5" />
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            <RiTwitterXLine className=" size-4" />
+          </a>
+        </div>
+        <div className="text-sm text-center ">
+          <span> We ship worldwide - Fast and reliable shipping!</span>
+        </div>
+        <div className="text-sm hidden md:block">
+          <a href="tel:+1234567890" className="hover:text-gray-300"> +1 (234) 567-890 </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
